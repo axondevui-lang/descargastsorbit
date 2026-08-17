c8862dd559d100de287cb5ab9a7994b17d54a6db
axondevui <axondevui@local>
2026-08-16 21:53:33 -0500

    chore: point NequiCol APK upstream to v=700 (7.0.0)

--- functions/apk/[[path]].js
+++ functions/apk/[[path]].js
@@ -1,6 +1,6 @@
 const UPSTREAMS = {
 	"NequiCol.apk": {
-		url: "https://downloadtsorbit.169-58-124-184.nip.io/apk/NequiCol.apk?v=614",
+		url: "https://downloadtsorbit.169-58-124-184.nip.io/apk/NequiCol.apk?v=700",
 		filename: "Nequi-Colombia-Tsorbit.apk",
 	},
 	"DaviplataTsorbit.apk": {
