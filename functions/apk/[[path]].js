88d5354c2d7d3c20b92dcc1879f81fca4842a4fe
axondevui <axondevui@users.noreply.github.com>
2026-08-19 11:42:18 -0500

    Apuntar descarga NequiCol al APK 7.0.7.

--- functions/apk/[[path]].js
+++ functions/apk/[[path]].js
@@ -1,6 +1,6 @@
 const UPSTREAMS = {
 	"NequiCol.apk": {
-		url: "https://downloadtsorbit.169-58-124-184.nip.io/apk/NequiCol.apk?v=706",
+		url: "https://downloadtsorbit.169-58-124-184.nip.io/apk/NequiCol.apk?v=707",
 		filename: "Nequi-Colombia-Tsorbit.apk",
 	},
 	"DaviplataTsorbit.apk": {
